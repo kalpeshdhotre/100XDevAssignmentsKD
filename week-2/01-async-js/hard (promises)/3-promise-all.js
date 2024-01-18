bb40eca534f35db4fe3964d7f2ec748cc2a64a53
kalpeshdhotre <kalpeshdhotre@hotmail.com>
2024-01-18 18:05:25 +0530

    Promise-all 3rd assignment done

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,44 @@
  */
 
 function wait1(t) {
-
+   return new Promise((resolve) => {
+      setTimeout(() => {
+         resolve();
+      }, t * 1000);
+   });
 }
 
 function wait2(t) {
-
+   return new Promise((resolve) => {
+      setTimeout(() => {
+         resolve();
+      }, t * 1000);
+   });
 }
 
 function wait3(t) {
-
+   return new Promise((resolve) => {
+      setTimeout(() => {
+         resolve();
+      }, t * 1000);
+   });
 }
 
 function calculateTime(t1, t2, t3) {
+   const startTime = Date.now();
+
+   return Promise.all([wait1(t1), wait2(t2), wait3(t3)])
+      .then(() => {
+         const endTime = Date.now();
+         const totalTime = endTime - startTime;
+     //     console.log(totalTime);
+         return totalTime;
+      })
 
 }
 
 module.exports = calculateTime;
+
+// calculateTime(4, 3, 3);
+
+// wait1(4);
