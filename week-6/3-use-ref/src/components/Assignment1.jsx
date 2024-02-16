900da9bb6339252d4dd67985d805a503bbba0080
kalpeshdhotre <kalpeshdhotre@hotmail.com>
2024-02-16 20:43:49 +0530

    Week 6 Hooks Assignment all Done useRef done

--- week-6/3-use-ref/src/components/Assignment1.jsx
+++ week-6/3-use-ref/src/components/Assignment1.jsx
@@ -1,21 +1,26 @@
+import { set } from "mongoose";
+import { useRef } from "react";
 import { useEffect } from "react";
 
 // Create a component with a text input field and a button. When the component mounts or the button is clicked, automatically focus the text input field using useRef.
 
 export function Assignment1() {
+   const setHere = useRef(null);
 
-    useEffect(() => {
+   //    This is to set focus on component load - this will run once since there is no dependency
+   useEffect(() => {
+      setHere.current.focus();
+   }, []);
 
-    }, []);
+   //    this is to set focus when button is clicked
+   const handleButtonClick = () => {
+      setHere.current.focus();
+   };
 
-    const handleButtonClick = () => {
-
-    };
-
-    return (
-        <div>
-            <input type="text" placeholder="Enter text here" />
-            <button onClick={handleButtonClick}>Focus Input</button>
-        </div>
-    );
-};
+   return (
+      <div>
+         <input type="text" placeholder="Enter text here" ref={setHere} />
+         <button onClick={handleButtonClick}>Focus Input</button>
+      </div>
+   );
+}
